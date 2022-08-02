dc19beea7c5bec993b7906ff0e1da39d5f21f3db
Andra Belu <andra.belu@3pillarglobal.com>
2022-08-02 12:48:57 +0300

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+const AboutPage = () =>{
+    return(
+        <main>
+            <title>Despre Mine</title>
+            <h1>Despre Mine</h1>
+            <p>Buna, eu sunt Belu Andra-Stefania! </p>
+        </main>
+    );
+};
+
+export default AboutPage;
