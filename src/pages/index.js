6b0b9257a27009bd96778f45646d41c05ddb283c
Andra Belu <andra.belu@3pillarglobal.com>
2022-08-04 12:21:42 +0300

    Finished Gatsby Tutorial Part 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import Layout from "../components/layout";
+import { StaticImage } from "gatsby-plugin-image";
 
 const Homepage = ()=>{
   return (
     <Layout pageHeading='Home' pageTitle='Home Page'>
       Welcome to my new Gatsby site!
+      <StaticImage src="../images/photoshoot.png" alt="Belu Andra-Stefania" />
     </Layout>
   )
 };
