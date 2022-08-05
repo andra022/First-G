e3f9ed8e0fd37e499280b6d257085fb34696fbac
Andra Belu <andra.belu@3pillarglobal.com>
2022-08-05 11:55:11 +0300

    Added content in the blog posts working with mdx plugin

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,29 +1,37 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import { graphql } from 'gatsby'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
 
 const BlogPage = ({ data }) => {
   return (
-    <Layout pageHeading='My Blog Posts' pageTitle="My Blog Posts">
-      <ul>
+    <Layout pageHeading='Blog Posts' pageTitle="My Blog Posts">
         {
-        data.allFile.nodes.map( node => (
-           <li key={node.name}>{node.name}</li>
+        data.allMdx.nodes.map( node => (
+           <article key={node.id}>
+            <h2>{node.frontmatter.Title}</h2>
+            <p>{node.frontmatter.Date}</p>
+            <MDXRenderer>{node.body}</MDXRenderer>
+           </article>
          ) )
         }
-      </ul>
     </Layout>
   )
 }
 
 export const query = graphql`
-  query {
-    allFile {
-      nodes {
-        name
+query MQuery {
+  allMdx(sort: {fields: frontmatter___Date, order: DESC}) {
+    nodes {
+      frontmatter {
+        Date(formatString: "MMMM D, YYYY")
+        Title
       }
+      id
+      body
     }
   }
+}
 `
 
 export default BlogPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,14 @@
 import * as React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 import Layout from "../components/layout";
 import { StaticImage } from "gatsby-plugin-image";
 
 const Homepage = ()=>{
   return (
     <Layout pageHeading='Home' pageTitle='Home Page'>
-      Welcome to my new Gatsby site!
+      Welcome to my new Gatsby site! 
+      <br></br>
+      <br></br>
       <StaticImage src="../images/photoshoot.png" alt="Belu Andra-Stefania" />
     </Layout>
   )
